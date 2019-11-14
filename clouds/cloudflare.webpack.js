2ecde1aedad1bc7428edc6556814a4ab30c13709
winton <mail@wintoni.us>
2019-11-13 21:59:17 -0500

    Minor cleanup

--- clouds/cloudflare.webpack.js
+++ clouds/cloudflare.webpack.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+
 const globby = require("globby")
 const path = require("path")
 
@@ -7,6 +8,7 @@ const mjs = globby.sync(path.join(__dirname, "../dist/mjs/*.mjs"))
 const rules = mjs.map(function(p) {
   const baseWithVersion = path.basename(p)
   const base = baseWithVersion.split(/-\d/)[0]
+  
   return {
     test: /\/stackComponent\.js$/,
     loader: "string-replace-loader",
