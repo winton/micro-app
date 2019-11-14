f3427bbdc31072da3095aa1a00aa98f9f1054405
winton <mail@wintoni.us>
2019-11-13 21:52:07 -0500

    Move lib mjs into dist as apart of build, replace mjs paths in webpack

--- clouds/cloudflare.webpack.js
+++ clouds/cloudflare.webpack.js
@@ -1,4 +1,25 @@
+/* eslint-disable */
+const globby = require("globby")
+const path = require("path")
+
+const mjs = globby.sync(path.join(__dirname, "../dist/mjs/*.mjs"))
+
+const rules = mjs.map(function(p) {
+  const baseWithVersion = path.basename(p)
+  const base = baseWithVersion.split(/-\d/)[0]
+  return {
+    test: /\/stackComponent\.js$/,
+    loader: "string-replace-loader",
+    options: {
+      search: "\"\\/[\\w\\/@-]+" + base + "(-\\*)?\\.mjs",
+      replace: "\"/" + baseWithVersion,
+      flags: "ig"
+    }
+  }
+})
+
 module.exports = {
   entry: "./clouds/cloudflare.js",
   target: "webworker",
+  module: { rules: rules }
 }
