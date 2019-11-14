f804fe2a0ae893066009ce0b30088fa9f2e511dc
winton <mail@wintoni.us>
2019-11-14 12:45:46 -0500

    Use cors-worker on cloudflare asset worker

--- clouds/cloudflare/assets.js
+++ clouds/cloudflare/assets.js
@@ -1,42 +1,18 @@
 /* eslint-disable */
 
+import cors from "@fn2/cors-worker"
+
 const assets = "http://micro-app.s3.amazonaws.com"
-
-const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, HEAD, POST, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type",
-}
-
-function handleOptions(request) {
-  const headers = request.headers
-
-  const addCors = headers.get("Origin") !== null &&
-    headers.get("Access-Control-Request-Method") !== null &&
-    headers.get("Access-Control-Request-Headers") !== null
-  
-  return new Response(null, {
-    headers: addCors ?
-      corsHeaders :
-      { "Allow": "GET, HEAD, POST, OPTIONS" }
-    }
-  )
-}
 
 async function handleRequest(req) {
   const url = new URL(req.url)
   const path = url.pathname
   
   if (req.method === "OPTIONS") {
-    return handleOptions(req)
+    return cors.handleOptions(req)
   } else {
     const res = await fetch(assets + path)
-    const response = new Response(res.body, res)
-
-    response.headers.set("Access-Control-Allow-Origin", "*")
-    response.headers.append("Vary", "Accept-Encoding")
-
-    return response
+    return cors.addCors(res)
   }
 }
 
